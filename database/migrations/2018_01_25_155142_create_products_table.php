14e96179ce5b8cce8ab7d262766d2d13176b0bce
hrmoore927 <hrmoore927@gmail.com>
2018-01-25 23:32:35 -0500

    Added user, prod, ord, pay, bill, ship, and status tables

---
+++ database/migrations/2018_01_25_155142_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('productName');
+            $table->string('description');
+            $table->string('materials');
+            $table->string('weight');
+            $table->string('dimensions');
+            $table->string('category');
+            $table->integer('cost');
+            $table->string('keywords');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- database/migrations/2018_01_25_155200_create_orders_table.php
+++ database/migrations/2018_01_25_155200_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('firstName');
-            $table->string('lastName');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('phone');
+            $table->integer('userID');
+            $table->integer('productID');
+            $table->integer('quantity');
+            $table->integer('total');
+            $table->integer('paymentID');
             $table->integer('billID');
             $table->integer('shipID');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('orderDate');
+            $table->string('shipDate');
+            $table->string('orderStatus');
         });
     }
 
@@ -34,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 }
